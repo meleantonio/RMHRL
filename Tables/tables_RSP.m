f78ba28e6cb9addf39dbe080def8a926d8e07136
Antonio Mele <meleantonio@gmail.com>
2017-08-28 09:42:26 +0100

    Tables: updated tables_RSP.m

--- Tables/tables_RSP.m
+++ Tables/tables_RSP.m
@@ -1,8 +1,9 @@
+
 clear all;
 
 global alpha betta sig epsil omega1 omega2 nu nu2 ;
-global output_y s1 s2 elast1 elast2 delta1 delta2 
-global  rounds_approx OrderVector Order_vector 
+global output_y s1 s2 elast1 elast2 delta1 delta2
+global  rounds_approx OrderVector Order_vector
 
 
 % PARAMETERS:
@@ -38,6 +39,17 @@ s2 = zeros(nba,1);
 s2(1) =  output_y-.45;
 s2(2) = .45;
 
+% grid for phi
+phibar = omega2/omega1;
+phi_sd =omega2/omega1;
+phi_min = phibar- .15*phi_sd;
+phi_max = phibar+  .15*phi_sd;
+
+% grid for k
+k_ss = 2.12;
+k_min =2;
+k_max =4; 
+
 
 OrderVector = [ 2  4  6 7 8 ;  2  4  6 7 8 ;2  4  6 7 8 ];
 time_computation = zeros(1,length(OrderVector));
@@ -53,10 +65,13 @@ for uu=1:length(OrderVector)
     disp(sprintf(' Grid is %g by %g by %g',Order_vector(1, rounds_approx),...
         Order_vector(2, rounds_approx),Order_vector(3, rounds_approx)));
     mainRSP;
+    time_computation_vec(uu) = time_computation;
+    testing_max_vec(uu) =  testing_max;
+    testing_norm_vec(uu) = testing_norm;
     
 end
 
 
-
-table_final_RSP = [OrderVector; time_computation; testing_max; testing_norm]
+%% Print table and save it
+table_final_RSP = [OrderVector(1,:); time_computation_vec; testing_max_vec; testing_norm_vec]'
 save table_final_RSP table_final_RSP -ascii -double -tabs
